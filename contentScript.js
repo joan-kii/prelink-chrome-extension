a9933e2d1fd9e4a20b364e369ffce2bdabd1f02e
Joan Serrano <59319203+joan-kii@users.noreply.github.com>
2022-06-19 19:13:55 +0200

    añade user settings

--- contentScript.js
+++ contentScript.js
@@ -13,14 +13,15 @@
       Object.assign(options, result.options);
     }
   });
+
+  // Reload extension after change options
+  chrome.storage.onChanged.addListener(() => {
+    window.location.reload();
+  });
   
   // Listen to messages from background
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    chrome.storage.sync.get('options', (result) => {
-      console.log(result.options)
-      // Seguir aquí (asignar classes y data-tootips según options)
-    })
-
+    
     // Get all links in page
     let links = document.querySelectorAll('a');
     
@@ -35,7 +36,13 @@
         if (href !== null && href.startsWith('h') && !href.includes(message.url)) {
 
           // Set class to link
-          link.classList.add('prelink');
+          if (options.color === 'multicolor') {
+            link.classList.add('multicolor');
+          } else if (options.color === 'red') {
+            link.classList.add('red');
+          } 
+
+          if (options.banner === 'hover') link.classList.add('hover');
 
           // Create tooltip text
           const certificate = href.startsWith('https') ? 
@@ -47,13 +54,10 @@
           // Get browser position link (avoid header top overflow)
           const linkX = link.offsetTop;
 
+          link.setAttribute('data-tooltip', tooltipText);
           // // Create data attributes in link according to position
-          if (linkX < 35) {
-            link.setAttribute('data-tooltip', tooltipText);
-            link.setAttribute('data-tooltip-position', 'bottom');
-          } else {
-            link.setAttribute('data-tooltip', tooltipText);
-          }
+          if (linkX < 35) link.setAttribute('data-tooltip-position', 'bottom');
+          link.setAttribute('data-tooltip', tooltipText); 
         }
       }
     } else if (message.action === 'OFF') {
@@ -61,8 +65,8 @@
       // Remove links data-tooltips and classes
       let prelinks = document.querySelectorAll('[data-tooltip]');
       for (let link of prelinks) {
-        link.removeAttribute('data-tooltip');
-        link.classList.remove('prelink');
+        link.removeAttribute('data-tooltip', 'data-tooltip-bottom');
+        link.classList.remove('multicolor', 'red', 'hover', 'fixed');
       }
     }
 
--- options.js
+++ options.js
@@ -4,20 +4,16 @@ const button = document.querySelector('#save');
 // Listen to click save button
 button.addEventListener('click', (e) => {
   e.preventDefault();
-
+  
   // Get settings values
   const colorSelect = document.querySelector('#color').value;
   const bannerSelect = document.querySelector('#banner').value;
-  const saved = document.querySelector('#message');
-
+  
   const options = {
     color: colorSelect,
     banner: bannerSelect
   };
 
   chrome.storage.sync.set({options});
-
-  // Show saved message to user
-  if (!saved.textContent) saved.append('Settings saved!');
-  saved.classList.remove('d-none');
+  close();
 });
